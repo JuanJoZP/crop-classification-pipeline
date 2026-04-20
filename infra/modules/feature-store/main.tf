8e6db637a0145350470046ef826bdb1d9de799d4
Juan Jose Zuluaga Patino <juanzpjose136@gmail.com>
2026-04-20 11:12:59 -0500

    feat: add indexes to feature store, fix List Types -> Serialized string

--- infra/modules/feature-store/main.tf
+++ infra/modules/feature-store/main.tf
@@ -1,11 +1,11 @@
 resource "aws_sagemaker_feature_group" "this" {
   feature_group_name             = var.feature_group_name
-  record_identifier_feature_name = "polygon_id"
+  record_identifier_feature_name = "objectid"
   event_time_feature_name        = "event_time"
   role_arn                       = var.feature_store_role_arn
 
   feature_definition {
-    feature_name = "polygon_id"
+    feature_name = "objectid"
     feature_type = "String"
   }
 
@@ -40,58 +40,150 @@ resource "aws_sagemaker_feature_group" "this" {
   }
 
   feature_definition {
-    feature_name = "label"
-    feature_type = "Integral"
+    feature_name = "metadata"
+    feature_type = "String"
+  }
+
+  # 13 Sentinel-2 bands
+  feature_definition {
+    feature_name = "coastal_series"
+    feature_type = "String"
   }
 
   feature_definition {
-    feature_name = "n_timesteps"
-    feature_type = "Integral"
+    feature_name = "blue_series"
+    feature_type = "String"
   }
 
   feature_definition {
-    feature_name = "n_pixels"
-    feature_type = "Integral"
+    feature_name = "green_series"
+    feature_type = "String"
   }
 
   feature_definition {
-    feature_name = "ndvi_mean"
-    feature_type = "Fractional"
+    feature_name = "red_series"
+    feature_type = "String"
   }
 
   feature_definition {
-    feature_name = "ndvi_std"
-    feature_type = "Fractional"
+    feature_name = "rededge1_series"
+    feature_type = "String"
   }
 
   feature_definition {
-    feature_name = "evi_mean"
-    feature_type = "Fractional"
+    feature_name = "rededge2_series"
+    feature_type = "String"
   }
 
   feature_definition {
-    feature_name = "evi_std"
-    feature_type = "Fractional"
+    feature_name = "rededge3_series"
+    feature_type = "String"
   }
 
   feature_definition {
-    feature_name = "gndvi_mean"
-    feature_type = "Fractional"
+    feature_name = "nir_series"
+    feature_type = "String"
   }
 
   feature_definition {
-    feature_name = "gndvi_std"
-    feature_type = "Fractional"
+    feature_name = "nir08_series"
+    feature_type = "String"
   }
 
   feature_definition {
-    feature_name = "ndmi_mean"
-    feature_type = "Fractional"
+    feature_name = "nir09_series"
+    feature_type = "String"
   }
 
   feature_definition {
-    feature_name = "ndmi_std"
-    feature_type = "Fractional"
+    feature_name = "swir16_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "swir22_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "aot_series"
+    feature_type = "String"
+  }
+
+  # 14 spectral indexes + veg_index
+  feature_definition {
+    feature_name = "veg_index_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "ndvi_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "evi_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "gndvi_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "savi_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "msavi_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "ndwi_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "gcvi_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "vari_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "ndre_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "cire_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "ndmi_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "mndwi_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "psri_series"
+    feature_type = "String"
+  }
+
+  feature_definition {
+    feature_name = "rendvi_series"
+    feature_type = "String"
   }
 
   offline_store_config {
